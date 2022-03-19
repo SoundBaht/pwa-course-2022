10aa4ef1eb935116846e87eb87df4c3839e5bc99
SoundBaht <chirayu.nork@bumail.net>
2022-03-19 10:49:54 +0700

    1630702460

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -6,7 +6,13 @@ function Userlist () {
 
   function onButtonClicked() {
     // ex01 filling logic here
-  }
+    if (trayStatus === 'open'){
+    setTrayStatus('close')
+    }if(trayStatus === 'close')
+    {
+    setTrayStatus('open')
+    }
+}
 
   return (
     <div className={"userlist "+trayStatus}>
